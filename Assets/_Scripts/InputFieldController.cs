f956cf8ef9064546c75332f1e9b816ecec6efde1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-01-06 13:27:02 -0800

    new scriptable objects

---
+++ Assets/_Scripts/InputFieldController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InputFieldController : MonoBehaviour
+{
+    [Header("---Resources---")]
+    [SerializeField]
+    private TMP_InputField fieldOne;
+
+    [SerializeField]
+    private TMP_InputField fieldTwo;
+
+    [SerializeField]
+    private TMP_InputField fieldThree;
+
+    /// <summary>
+    /// Extract input text from each field.
+    /// </summary>
+    public string[] Answers { get => new string[]
+    {fieldOne.text, fieldTwo.text, fieldThree.text};}
+}
---
+++ Assets/_Scripts/_Scriptable Objects/RoundAnswers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoundAnswers",
+	menuName = "ScriptableObjects/RoundAnswers")]
+public class RoundAnswers : ScriptableObject
+{
+    [SerializeField]
+    private List<string> field1Answers = new List<string>();
+    public List<string> Field1Answers { get => field1Answers; }
+
+    [SerializeField]
+    private List<string> field2Answers = new List<string>();
+    public List<string> Field2Answers { get => field2Answers; }
+
+    [SerializeField]
+    private List<string> field3Answers = new List<string>();
+    public List<string> Field3Answers { get => field3Answers; }
+}
