8bea9cc1fe79f38f6dd031a583c77b665974fdcc
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-01-06 22:29:26 -0800

    UI timer

---
+++ Assets/_Scripts/UITimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+using TMPro;
+
+public class UITimer : MonoBehaviour
+{
+
+    private const string COLON = ":";
+    [Header("---Resources---")]
+    [SerializeField]
+    private TextMeshProUGUI TMPText;
+
+    [SerializeField]
+    private FloatReference timeValue;
+
+    private StringBuilder myStringBuilder = new StringBuilder(4);
+
+    private void OnEnable()
+    {
+        timeValue.AddListener(UpdateUI);
+    }
+    
+    private void OnDisable()
+    {
+        timeValue.RemoveListener(UpdateUI);
+    }
+
+    private void UpdateUI()
+    {
+        //format for time
+        myStringBuilder.Clear();
+        myStringBuilder.Append((int)(timeValue.Value / 60));//minutes
+        myStringBuilder.Append(COLON);
+        myStringBuilder.Append((int)(timeValue.Value % 60));//seconds
+
+        TMPText.text = myStringBuilder.ToString();
+    }
+
+
+}
